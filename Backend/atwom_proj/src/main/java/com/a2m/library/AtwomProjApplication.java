3037e68431acf0accc2ed9a39763d6b8e496f19d
Viviorigi <duong682@gmail.com>
2024-08-02 21:45:04 +0700

    add fd

---
+++ Backend/atwom_proj/src/main/java/com/a2m/library/AtwomProjApplication.java
@@ -0,0 +1,13 @@
+package com.a2m.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtwomProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtwomProjApplication.class, args);
+	}
+
+}
---
+++ Backend/atwom_proj/src/test/java/com/a2m/library/AtwomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2m.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtwomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
