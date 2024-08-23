bd4c29a2d647761ad703c3e942701cbefaa7bf31
Viviorigi <duong682@gmail.com>
2024-08-23 20:56:15 +0700

    fix img

--- BackEnd/atwom_proj/src/main/java/com/a2m/library/repository/ImagesBookRepository.java
+++ BackEnd/atwom_proj/src/main/java/com/a2m/library/repository/ImagesBookRepository.java
@@ -2,10 +2,12 @@ package com.a2m.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.a2m.library.model.ImagesBook;
 
 @Repository
 public interface ImagesBookRepository extends JpaRepository<ImagesBook, Integer>{
-
+	@Transactional
+    void deleteByBookId(Integer bookId);
 }
