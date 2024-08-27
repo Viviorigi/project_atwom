c72cf5a4cd4b31c267db13e3624f9e0b3f825270
PhongThanQuang <phongfitq@gmail.com>
2024-08-27 18:38:55 +0700

    order

---
+++ BackEnd/atwom_proj/src/main/java/com/a2m/library/dto/response/CheckoutListResponse.java
@@ -0,0 +1,19 @@
+package com.a2m.library.dto.response;
+
+import java.util.List;
+
+import com.a2m.library.dto.CheckoutDTO;
+import com.a2m.library.model.Banner;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CheckoutListResponse {
+	private List<CheckoutDTO> notis;
+	private int totalPages;
+	private long totalCheckouts;
+}
